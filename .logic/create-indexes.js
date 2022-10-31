8557c6c2376bf1f676b678418aa6b1f6f818b91d
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2022-10-31 09:09:51 +0330

    feat: indexes

---
+++ .logic/create-indexes.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { join } from 'path';
+import glob from 'glob';
+import { writeFileSync } from 'fs';
+
+const languages = ['en', 'fa'];
+
+for (const language of languages) {
+  console.log(`Processing *.${language}.yaml files`);
+  const list = glob.sync(`**/*.${language}.yaml`, {
+    cwd: join(process.cwd(), '..'),
+  });
+  const words = list.map((item) => item.split('/').reverse()[1]).sort();
+  writeFileSync(`./en-to-${language}.index.csv`, words.join('\n'), 'utf-8');
+}
+
+debugger;
